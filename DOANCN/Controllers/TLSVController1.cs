cd5c9ceea373ce3901386a92f4d98d4b88cf8478
Trương Xuân Nguyên <215748020110162@vinhuni.edu.vn>
2024-05-17 10:03:35 +0700

    17/5

---
+++ DOANCN/Controllers/TLSVController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DOANCN.Controllers
+{
+    public class TLSVController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DOANCN/RequireLoginAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+
+namespace DOANCN
+{
+    public class RequireLoginAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var userID = context.HttpContext.Session.GetLong("ID");
+            if (!userID.HasValue)
+            {
+                context.Result = new RedirectToActionResult("Index", "Login", null);
+            }
+        }
+    }
+}
